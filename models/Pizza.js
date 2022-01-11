51e75fc80ad6ce747d7d765e619dcf610a8acd4e
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-10 19:01:28 -0700

    comment schema and virtual

--- models/Pizza.js
+++ models/Pizza.js
@@ -15,8 +15,30 @@ const PizzaSchema = new Schema({
         type: String,
         default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+        {
+            //tell mongoose to expect an object id and that the data comes from the comment model 
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+}
+);
+
+//aDD A VIRTUAL- allow us to add more info to a database response so we dont add info manually with a helper 
+//this is in pizza because we only care about comment sin relation to pizza
+//virtuals let you add virtual properties to a document that arent stored in the database
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
+
 
 
 //create the pizza model using the pizzaschame
